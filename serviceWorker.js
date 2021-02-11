e261b7cb0b017d1dfcde6f0b2594a17bb5ea7cdd
fazeSenit <afandziho@gmail.com>
2021-02-11 18:47:10 +0100

    Made by Afan Dziho
    Lalalala

---
+++ serviceWorker.js
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title> onload script </title>
+<script language="javascript">
+function done() {
+  alert("stranica je učitana")
+}
+</script>
+<body onload="done()">
+</script>
+tekst tela dokumenata
+</body>
+</html>
+
